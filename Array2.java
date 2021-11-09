50351543c5ebeca5e6c727da082d1fcd760e2d8e
vishalvermait651 <vishalvermait651@gmail.com>
2021-11-08 21:52:52 -0500

    added Array java

---
+++ Array2.java
@@ -0,0 +1,9 @@
+
+public class Array2 {
+
+	static int[] get(){                           //creating method which returns an array  
+
+		return new int[] {10,20,30,40,50,60};
+	}
+
+}
---
+++ Array3.java
@@ -0,0 +1,12 @@
+
+public class Array3 extends Array2 {
+
+	public static void main(String[] args) {
+
+		int arr[] = get();                                 //calling method which returns an array  
+
+		for (int i = 0; i < arr.length; i++)               //calling method which returns an array  
+			System.out.println(arr[i]);
+	}
+
+}
